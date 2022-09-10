78ce334b25a75c61b39483e2ce0cf03e2ac80739
Nahom Mulugeta <nsolomon247@icloud.com>
2022-09-09 20:23:57 -0500

    updates

---
+++ env/pythonprovision.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo ---- Updating Sources ----
+sudo apt-get update -y
+
+echo ---- Installing Python ----
+sudo apt-get install software-properties-common -y
+
+echo --- Current Python3 Version ---
+python3 --version
+
+echo --- Initialization Complete ---
+
+
+
+
+
+
+
+
+
+
+
+
